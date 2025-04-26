b0af88c45f9a0cba64fae6bddae763e5a6a7a681
Muhammad Arfan <arfan2173@gmail.com>
2025-04-27 03:47:52 +0700

    created response helpers

---
+++ app/helpers/response_helpers.py
@@ -0,0 +1,16 @@
+def create_response_body (status: int, message: str, data=None, additionals=None):
+    body = {
+        "status": status,
+        "message": message,
+    }
+
+    if data is not None:
+        body["data"] = data
+
+    if additionals is not None:
+        body.update(additionals)
+
+    return body
+
+def create_response_tuple (status: int, message: str, data=None, additionals=None):
+    return create_response_body(status, message, data, additionals), status
