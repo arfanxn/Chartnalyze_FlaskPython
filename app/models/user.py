64553d4fc25d34d191287b98aa5121a5f425aaf1
Muhammad Arfan <arfan2173@gmail.com>
2025-04-26 19:30:09 +0700

    added new column

--- app/models/user.py
+++ app/models/user.py
@@ -6,10 +6,10 @@ import ulid
 class User(db.Model):
     __tablename__ = 'users'
 
-
     id = db.Column(db.CHAR(26), primary_key=True, default=lambda: ulid.new().str)
-    name = db.Column(db.String(50), nullable=False)
-    birth_date = db.Column(db.Date, nullable=False)
+    name = db.Column(db.String(50), nullable=True)
+    username = db.Column(db.String(16), nullable=False)
+    birth_date = db.Column(db.Date, nullable=True)
     email = db.Column(db.String(50), nullable=False, unique=True)
     email_verified_at = db.Column(db.DateTime, nullable=True)
     password = db.Column(db.String(255), nullable=False)
