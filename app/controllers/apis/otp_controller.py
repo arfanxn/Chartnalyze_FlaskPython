d816614b57ca289214ad7f9d900ee9230559d8ff
Muhammad Arfan <arfan2173@gmail.com>
2025-04-28 21:19:23 +0700

    only allows to send one otp per minute

---
+++ app/controllers/apis/otp_controller.py
@@ -0,0 +1,17 @@
+from app.services.otp_service import OtpService 
+from app.extensions import limiter
+from app.forms.send_otp_form import SendOtpForm
+from app.helpers.response_helpers import create_response_tuple
+from flask import Blueprint, request
+from http import HTTPStatus
+
+otp_service = OtpService()
+otp_bp = Blueprint('otp', __name__)
+
+@otp_bp.route('/otps/send', methods=['POST'])
+@limiter.limit("1 per minute")
+def send():
+    form = SendOtpForm(request.form)
+    form.try_validate()
+    otp_service.send(form.email.data)
+    return create_response_tuple(status=HTTPStatus.OK, message=f"OTP sent successfully")
