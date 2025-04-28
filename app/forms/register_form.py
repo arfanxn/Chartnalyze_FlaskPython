38836af9c22b1241a6d99b7ff4de8451c0a5ae1c
Muhammad Arfan <arfan2173@gmail.com>
2025-04-28 17:37:22 +0700

    updated rules

--- app/forms/register_form.py
+++ app/forms/register_form.py
@@ -1,33 +1,10 @@
-from wtforms import Form, StringField , EmailField, PasswordField, validators 
+from wtforms import Form, StringField, DateField, EmailField, PasswordField, validators 
 
 class RegisterForm(Form):
-    username = StringField(
-        'Username', 
-        [
-            validators.DataRequired(), 
-            validators.Length(min=2, max=16)
-        ]
-    )
-    email = EmailField(
-        'Email', 
-        [
-            validators.DataRequired(), 
-            validators.Email()
-        ]
-    )
-    password = PasswordField(
-        'Password', 
-        [
-            validators.DataRequired(), 
-            validators.Length(min=8, max=50)
-        ]
-    )
-    confirm_password = PasswordField(
-        'Confirm Password', 
-        [
-            validators.DataRequired(), 
-            validators.Length(min=8, max=50), 
-            validators.EqualTo('password')
-        ]
-    )
+    name = StringField('Name', [validators.DataRequired(), validators.Length(min=2, max=50)])
+    username = StringField('Username', [validators.DataRequired(), validators.Length(min=2, max=16)])
+    birth_date = DateField('Birth Date', [validators.DataRequired()], format='%Y-%m-%d')
+    email = EmailField('Email', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('Password', [validators.DataRequired(), validators.Length(min=8, max=50)])
+    confirm_password = PasswordField('Confirm Password', [validators.DataRequired(), validators.Length(min=8, max=50), validators.EqualTo('password')])
 
