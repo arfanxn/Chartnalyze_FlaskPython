270283005cec7363b12767d11a589669cf867d06
Muhammad Arfan <arfan2173@gmail.com>
2025-04-28 22:11:41 +0700

    updated how otps table works

--- database/migrations/versions/7d5428f57960_create_otps_table.py
+++ database/migrations/versions/7d5428f57960_create_otps_table.py
@@ -19,7 +19,7 @@ depends_on = None
 def upgrade():
     op.create_table('otps',
         sa.Column('id', sa.CHAR(length=26), primary_key=True),
-        sa.Column('user_id', sa.CHAR(length=26), nullable=False),
+        sa.Column('email', sa.String(length=50), nullable=False),
         sa.Column('code', sa.Integer, nullable=False),  # 6 digits OTP code, don't start with 0 to avoid truncation
         sa.Column('used_at', sa.DateTime, nullable=True),
         sa.Column('revoked_at', sa.DateTime, nullable=True),
@@ -27,8 +27,6 @@ def upgrade():
         sa.Column('created_at', sa.DateTime, nullable=False, default=sa.func.now()),
         sa.Column('updated_at', sa.DateTime),
     )
-
-    op.create_foreign_key('otps_user_id_fkey', 'otps', 'users', ['user_id'], ['id'], ondelete='CASCADE', onupdate='CASCADE')
     pass
 
 
