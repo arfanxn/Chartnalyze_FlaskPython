de68c37caa7685093a26c62b9ec02bb51a6eb151
Muhammad Arfan <arfan2173@gmail.com>
2025-04-29 19:45:14 +0700

    updated test controller

--- app/controllers/test_controller.py
+++ app/controllers/test_controller.py
@@ -1,20 +1,16 @@
 from flask import Blueprint, request, jsonify
-from datetime import  date
 from app.models.user import User
-from  app.extensions import db
+from app.extensions import db
+from app.helpers.response_helpers import create_response_tuple
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
+from datetime import  date
+from http import HTTPStatus
+
 
 test_bp = Blueprint('test', __name__)
 
 @test_bp.route('/test', methods=['GET'])
 def test():
-    user = User()
-    user.name = "test"
-    user.birth_date = date(2000, 1, 1)
-    user.email = "test@test.com" 
-    user.set_password("11112222")
-
-    db.session.add(user)
-    db.session.commit()
-
-    return "test", 200
+    user = User.query.first()
+    return create_response_tuple(status=HTTPStatus.OK, message='User found successfully', data={'user': user.to_json()})
+    
