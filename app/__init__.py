02cbb475aa9f872821b7647aabf02decc7b60e26
Muhammad Arfan <arfan2173@gmail.com>
2025-04-29 22:36:04 +0700

    register commands

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from app.extensions import db, jwt, limiter, mail, migrate
 from app.config import Config
 from app.controllers import register_blueprints
+from app.commands import register_commands
 
 def create_app():
     app = Flask(__name__)
@@ -13,6 +14,9 @@ def create_app():
     limiter.init_app(app)
     mail.init_app(app)
     migrate.init_app(app, db)
+
+    # Register commands
+    register_commands(app)
 
     # Register Blueprints
     register_blueprints(app)
