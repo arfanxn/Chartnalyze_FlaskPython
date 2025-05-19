607cf54c49eb7340c41d977205d39abd7f43e895
Muhammad Arfan <arfan2173@gmail.com>
2025-05-20 02:01:00 +0700

    registered model in init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,7 +3,9 @@ from .role_user import RoleUser
 from .permission_role import PermissionRole
 
 # Then import other models
+from .country import Country
 from .user import User
 from .role import Role
 from .permission import Permission
 from .otp import Otp
+from .notification import Notification
