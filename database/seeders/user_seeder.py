0a6b29c4895b322746e7f9fae4cfa1daa133c032
Muhammad Arfan <arfan2173@gmail.com>
2025-05-19 13:55:31 +0700

    updated user seeder to support country

--- database/seeders/user_seeder.py
+++ database/seeders/user_seeder.py
@@ -1,5 +1,5 @@
 from app.extensions import db
-from app.models import User, Role
+from app.models import User, Country, Role
 from database.seeders.seeder import Seeder
 from datetime import date
 
@@ -8,25 +8,32 @@ class UserSeeder(Seeder):
     def run(self):
         super().run()
 
+        indonesia_country = Country.query.filter_by(iso_code='ID').first()
+
         roles = Role.query.all()
+        root_role = [role for role in roles if role.name == 'root'][0]
+        analyst_role = [role for role in roles if role.name == 'analyst'][0]
+        user_role = [role for role in roles if role.name == 'user'][0]
+
 
         user_list = [
             {
                 'username': 'root',
-                'roles': [role for role in roles if role.name == 'root']
+                'roles': [root_role]
             },
             {
                 'username': 'analyst',
-                'roles': [role for role in roles if role.name == 'analyst']
+                'roles': [analyst_role]
             },
             {
                 'username': 'user',
-                'roles': [role for role in roles if role.name == 'user']
+                'roles': [user_role]
             }
         ]
 
         for user_dict in user_list: 
             user = User()
+            user.country_id = indonesia_country.id
             user.name = user_dict['username'].capitalize()
             user.username = user_dict['username']
             user.birth_date = date(2000, 1, 1)
