5bf55bef3bc666b1ccd75e03896c5396df86ff9f
Muhammad Arfan <arfan2173@gmail.com>
2025-05-20 02:02:16 +0700

    registered CountrySeeder and NotificationSeeder

--- database/seeders/__init__.py
+++ database/seeders/__init__.py
@@ -1,12 +1,16 @@
+from database.seeders.country_seeder import CountrySeeder
 from database.seeders.permission_seeder import PermissionSeeder
 from database.seeders.role_seeder import RoleSeeder
 from database.seeders.user_seeder import UserSeeder
+from database.seeders.notification_seeder import NotificationSeeder
 from database.seeders.seeder import Seeder
 
 _registered_seeders: list[Seeder] = [
+    CountrySeeder(),
     PermissionSeeder(),
     RoleSeeder(),
     UserSeeder(),
+    NotificationSeeder(),
 ]
 
 def run_registered_seeders ():
