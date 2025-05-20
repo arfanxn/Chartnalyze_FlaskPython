8a6b694745daeac9e742a5e41e672b2b98b2c901
Muhammad Arfan <arfan2173@gmail.com>
2025-05-21 00:38:41 +0700

    minor update

--- database/migrations/versions/e9111394ed36_create_saves_table.py
+++ database/migrations/versions/e9111394ed36_create_saves_table.py
@@ -9,6 +9,8 @@ from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
+from app.enums.save_enums import SaveableType
+
 # revision identifiers, used by Alembic.
 revision = 'e9111394ed36'
 down_revision = '4c3237406d60'
@@ -20,8 +22,8 @@ def upgrade():
     op.create_table('saves',
         sa.Column('id', sa.CHAR(26), primary_key=True),
         sa.Column('user_id', sa.CHAR(26), sa.ForeignKey('users.id'), nullable=False),
-        sa.Column('saveable_id', sa.CHAR(26), sa.ForeignKey('posts.id'), nullable=False),
-        sa.Column('saveable_type', sa.Enum('post', name='saveable_type'), nullable=False),
+        sa.Column('saveable_id', sa.CHAR(26), nullable=False),
+        sa.Column('saveable_type', sa.Enum(*[e.value for e in SaveableType], name='saveable_types'), nullable=False),
         sa.Column('created_at', sa.DateTime, nullable=False, server_default=sa.func.now()),
         sa.Column('updated_at', sa.DateTime, server_onupdate=sa.func.now())
     )
