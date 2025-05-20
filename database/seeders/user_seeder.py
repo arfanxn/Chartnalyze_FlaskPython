e8ccf43753fbabb6277ff980fc6471e2d5889740
Muhammad Arfan <arfan2173@gmail.com>
2025-05-20 17:31:22 +0700

    updated users seeder

--- database/seeders/user_seeder.py
+++ database/seeders/user_seeder.py
@@ -1,45 +1,45 @@
 from app.extensions import db
-from app.models import User, Country, Role
+from app.models import User, Country, Role, RoleUser
+from app.enums.role_enums import RoleName
 from database.seeders.seeder import Seeder
+from faker import Faker
+import ulid
 from datetime import date
+
+fake = Faker()
 
 class UserSeeder(Seeder):
 
     def run(self):
         super().run()
 
-        indonesia_country = Country.query.filter_by(iso_code='ID').first()
-
+        countries = db.session.query(Country.id).all()
         roles = Role.query.all()
-        root_role = [role for role in roles if role.name == 'root'][0]
-        analyst_role = [role for role in roles if role.name == 'analyst'][0]
-        user_role = [role for role in roles if role.name == 'user'][0]
 
+        users = []
 
-        user_list = [
-            {
-                'username': 'root',
-                'roles': [root_role]
-            },
-            {
-                'username': 'analyst',
-                'roles': [analyst_role]
-            },
-            {
-                'username': 'user',
-                'roles': [user_role]
-            }
-        ]
+        for i in range(len(roles)):
+            role = roles[i]
 
-        for user_dict in user_list: 
-            user = User()
-            user.country_id = indonesia_country.id
-            user.name = user_dict['username'].capitalize()
-            user.username = user_dict['username']
-            user.birth_date = date(2000, 1, 1)
-            user.email = user_dict['username']+'@chartnalyze.edu'
-            user.password = '11112222'
-            user.roles.extend(user_dict['roles'])
-            db.session.add(user)        
+            for j in range(0, 10):
+                country_id = fake.random_element(elements=countries).id if fake.boolean() else None
+                email_verified_at = fake.date_time_between(start_date='-1y', end_date='now') if fake.boolean() else None
 
-        db.session.commit()
+                user = User()
+                user.country_id = country_id
+                user.name = fake.name()
+                user.username = fake.user_name()[:16]
+                user.birth_date = fake.date_of_birth()
+                user.email = fake.email()
+                user.email_verified_at = email_verified_at
+                user.password = '11112222'   
+                user.roles.extend([role]) 
+
+                users.append(user)
+
+        try:
+            db.session.add_all(users)
+            db.session.commit()
+        except Exception as e:
+            db.session.rollback()
+            raise e
