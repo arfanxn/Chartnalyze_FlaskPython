bc2706372a1c35596bc42525a059a6a97cf70c0c
Muhammad Arfan <arfan2173@gmail.com>
2025-05-20 18:53:25 +0700

    registered the follow seeder

--- database/seeders/__init__.py
+++ database/seeders/__init__.py
@@ -3,6 +3,7 @@ from database.seeders.permission_seeder import PermissionSeeder
 from database.seeders.role_seeder import RoleSeeder
 from database.seeders.user_seeder import UserSeeder
 from database.seeders.notification_seeder import NotificationSeeder
+from database.seeders.follow_seeder import FollowSeeder
 from database.seeders.seeder import Seeder
 
 _registered_seeders: list[Seeder] = [
@@ -11,6 +12,7 @@ _registered_seeders: list[Seeder] = [
     RoleSeeder(),
     UserSeeder(),
     NotificationSeeder(),
+    FollowSeeder()
 ]
 
 def run_registered_seeders ():
