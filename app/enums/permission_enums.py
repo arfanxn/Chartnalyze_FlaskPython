6abfb166154aeab6fb46f58b60cfe536ca6e1360
Muhammad Arfan <arfan2173@gmail.com>
2025-05-21 22:37:28 +0700

    created PermissionName

---
+++ app/enums/permission_enums.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+class PermissionName(Enum):
+    ALL = '*'
+
+    USERS_INDEX = 'users.index'
+    USERS_SHOW = 'users.show'
+    USERS_STORE = 'users.store'
+    USERS_UPDATE = 'users.update'
+    USERS_DESTROY = 'users.destroy'
+
+    ROLES_INDEX = 'roles.index'
+    ROLES_SHOW = 'roles.show'
+    ROLES_STORE = 'roles.store'
+    ROLES_UPDATE = 'roles.update'
+    ROLES_DESTROY = 'roles.destroy'
+
+    PERMISSIONS_INDEX = 'permissions.index'
+    PERMISSIONS_SHOW = 'permissions.show'
+    PERMISSIONS_STORE = 'permissions.store'
+    PERMISSIONS_UPDATE = 'permissions.update'
+    PERMISSIONS_DESTROY = 'permissions.destroy'
