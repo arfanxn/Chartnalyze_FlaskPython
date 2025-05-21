e972a11d9f17449f073e08fdd84bbf439604a5ab
Muhammad Arfan <arfan2173@gmail.com>
2025-05-21 21:49:50 +0700

    refactored

--- app/services/otp_service.py
+++ app/services/otp_service.py
@@ -1,14 +1,7 @@
 from app.services import Service
-from app.extensions import db
-from app.models.otp import Otp
-from app.exceptions import HttpException
 from app.helpers.mail_helpers import send_mail
-from app.actions import SendOtpAction, VerifyOtpAction
+from app.actions import SendOtpAction
 from app.forms import SendOtpForm
-from app.config import Config
-from sqlalchemy import and_
-from datetime import datetime
-from http import HTTPStatus
 
 class OtpService(Service):
 
@@ -18,6 +11,3 @@ class OtpService(Service):
     def send(self, form: SendOtpForm):
         send_otp = SendOtpAction()
         send_otp(email=form.email.data)
-
-        db.session.commit()
-
