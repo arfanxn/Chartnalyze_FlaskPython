583c01c3bece932d3260bc1e3ffb786c948a73a0
Muhammad Arfan <arfan2173@gmail.com>
2025-05-22 22:12:50 +0700

    serve_public

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,11 +1,21 @@
-# from app.controllers.test_controller import test_bp
 from app.controllers.apis import api_bp
 from app.controllers.test_controller import test_bp
-from flask import Blueprint
+from app.helpers.file_helpers import get_public_folder_path
+from flask import Blueprint, send_from_directory
+
+# Create web blueprint
+web_bp = Blueprint('web', __name__, url_prefix='/')
+
+# Register module blueprints under web blueprint
+web_bp.register_blueprint(test_bp)
+
+@web_bp.route('/public/<path:filename>', methods=['GET'])
+def serve_public(filename):
+    return send_from_directory(get_public_folder_path(), filename)
 
 def register_blueprints(app):
     # Register test blueprint directly
-    app.register_blueprint(test_bp)
+    app.register_blueprint(web_bp)
     
     # Register API blueprint
     app.register_blueprint(api_bp)
