292885696915260d870a34b7364cf747f6a683d5
Muhammad Arfan <arfan2173@gmail.com>
2025-05-23 01:50:22 +0700

    created user resource with its relation loaded detecton

--- app/resources/user_resource.py
+++ app/resources/user_resource.py
@@ -8,19 +8,21 @@ class UserResource(Resource):
         self.model = model
 
     def to_json(self):
+        model = self.model
+
         data = {
-            'id': self.id,
-            'country_id': self.country_id,
-            'name': self.name,
-            'username': self.username,
-            'birth_date': self.birth_date.isoformat() if self.birth_date is not None else None,
-            'email': self.email,
-            'email_verified_at': self.email_verified_at.isoformat() if self.email_verified_at is not None else None,
-            'created_at': self.created_at.isoformat(),
-            'updated_at': self.updated_at.isoformat() if self.updated_at is not None else None,
+            'id': model.id,
+            'country_id': model.country_id,
+            'name': model.name,
+            'username': model.username,
+            'birth_date': model.birth_date.isoformat() if model.birth_date is not None else None,
+            'email': model.email,
+            'email_verified_at': model.email_verified_at.isoformat() if model.email_verified_at is not None else None,
+            'created_at': model.created_at.isoformat(),
+            'updated_at': model.updated_at.isoformat() if model.updated_at is not None else None,
         }
 
-        ins = inspect(self.model)
+        ins = inspect(model)
         if 'avatar' not in ins.unloaded:
             avatar = self.avatar
             data['avatar_url']  = f"{Config.APP_URL}/public/images/avatars/{avatar.file_name}"
