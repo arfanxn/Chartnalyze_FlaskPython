018946093c12365f623ecd452f7624e9f5ac6795
Muhammad Arfan <arfan2173@gmail.com>
2025-05-22 22:11:41 +0700

    updated

--- app/resources/resource.py
+++ app/resources/resource.py
@@ -1,13 +1,13 @@
 class Resource: 
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, model):
+        self.model = model
 
     def __getattr__(self, attr):
-        return getattr(self.data, attr)
+        return getattr(self.model, attr)
 
     def to_json(self):
         raise NotImplementedError
 
     @classmethod
-    def collection(cls, resources):
-        return [cls(item).to_json() for item in resources]
+    def collection(cls, models):
+        return [cls(model).to_json() for model in models]
