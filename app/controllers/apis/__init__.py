e96beb462638058c4d489771c3a19e9d859ed24e
Muhammad Arfan <arfan2173@gmail.com>
2025-05-23 12:35:29 +0700

    registered follow blueprint

--- app/controllers/apis/__init__.py
+++ app/controllers/apis/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from .user_controller import user_bp
 from .otp_controller import otp_bp
+from .follow_controller import follow_bp    
 from app.exceptions import ValidationException, HttpException
 from app.helpers.response_helpers import create_response_tuple
 from http import HTTPStatus
@@ -9,8 +10,9 @@ from http import HTTPStatus
 api_bp = Blueprint('api', __name__, url_prefix='/api')
 
 # Register module blueprints under API blueprint
-api_bp.register_blueprint(user_bp)
 api_bp.register_blueprint(otp_bp)
+api_bp.register_blueprint(user_bp)
+api_bp.register_blueprint(follow_bp)
 
 # Register error handlers for the API blueprint
 @api_bp.errorhandler(ValidationException)
