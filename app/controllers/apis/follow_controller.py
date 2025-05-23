6a165812f51333dba7e29984abf8ff886059f129
Muhammad Arfan <arfan2173@gmail.com>
2025-05-23 12:38:00 +0700

    implemented follow  related functionalities

---
+++ app/controllers/apis/follow_controller.py
@@ -0,0 +1,68 @@
+from app.middlewares import api_key_verified, authenticated, authorized, email_verified
+from app.resources import UserResource
+from app.services import FollowService 
+from app.extensions import limiter
+from app.forms import QueryForm
+from app.helpers.response_helpers import create_response_tuple
+from flask import Blueprint, request, g
+from http import HTTPStatus
+
+follow_service = FollowService()
+
+follow_bp = Blueprint('follow', __name__)
+
+@follow_bp.route('/users/<string:user_id>/followers', methods=['GET'])
+@api_key_verified
+@authenticated
+@authenticated
+@email_verified
+def followers_index(user_id: str):
+    form = QueryForm(request.args)
+    form.try_validate()
+
+    users, meta = follow_service.followers_index(form=form, user_id=user_id)
+    users_json = UserResource.collection(users)
+    users_pagination = {'users': users_json, **meta}
+
+    return create_response_tuple(
+        status=HTTPStatus.OK,
+        message='Followers paginated successfully',
+        data={ **users_pagination }
+    )
+
+
+@follow_bp.route('/users/<string:user_id>/followeds', methods=['GET'])
+@api_key_verified
+@authenticated
+@authenticated
+@email_verified
+def followeds_index(user_id: str):
+    form = QueryForm(request.args)
+    form.try_validate()
+
+    users, meta = follow_service.followeds_index(form=form, user_id=user_id)
+    users_json = UserResource.collection(users)
+    users_pagination = {'users': users_json, **meta}
+
+    return create_response_tuple(
+        status=HTTPStatus.OK,
+        message='Followeds paginated successfully',
+        data={ **users_pagination }
+    )
+
+@follow_bp.route('/users/self/toggle-follow/<followed_id>', methods=['PATCH'])
+@api_key_verified
+@authenticated
+@authenticated
+@email_verified
+def toggle_follow (followed_id: str):
+    follower_id = g.user.id
+
+    is_following = follow_service.toggle_follow(follower_id=follower_id, followed_id=followed_id)
+
+    message = 'Followed successfully' if is_following else 'Unfollowed successfully'
+
+    return create_response_tuple(
+        status=HTTPStatus.OK,
+        message=message
+    )
---
+++ app/services/follow_service.py
@@ -0,0 +1,90 @@
+from app.exceptions import HttpException
+from app.services import Service
+from app.models import Follow, User
+from app.forms import QueryForm
+from app.extensions import db
+from sqlalchemy.orm import aliased
+from http import HTTPStatus
+
+class FollowService(Service):
+
+    def __init__(self):
+        super().__init__()
+
+    def index(
+        self,
+        query_form: QueryForm,
+        user_id: str,
+        only_followers: bool = False,
+        only_followeds: bool = False
+    ) -> tuple[(list[Follow] | list[User]), dict]:
+        query = Follow.query
+
+        if only_followers:
+            query = query.join(User, Follow.follower_id == User.id).filter(Follow.followed_id == user_id)
+        elif only_followeds:
+            query = query.join(User, Follow.followed_id == User.id).filter(Follow.follower_id == user_id)
+
+        query = query.order_by(Follow.created_at.desc())
+
+        if query_form.keyword.data is not None:
+            query = query.filter(
+                db.or_(User.name.like(f'%{query_form.keyword.data}%'), User.email.like(f'%{query_form.keyword.data}%'))
+            )
+
+        pagination = query.paginate(
+            page=query_form.page.data,
+            per_page=query_form.per_page.data,
+            error_out=False,
+        )
+
+        items_length = len(pagination.items)
+
+        if items_length == 0:
+            raise HttpException(message='Users not found', status=HTTPStatus.NOT_FOUND)
+
+        meta = {
+            'page': pagination.page,
+            'per_page': pagination.per_page,
+            'total_pages': pagination.pages,
+            'has_prev': pagination.has_prev,
+            'has_next': pagination.has_next,
+            'total': pagination.total,
+        }
+
+        follows = pagination.items
+
+        if only_followers:
+            users = [follow.follower for follow in follows]
+            return users, meta
+        elif only_followeds:
+            users = [follow.followed for follow in follows]
+            return users, meta
+        else:
+            return follows, meta
+
+
+    def followers_index(self, form: QueryForm, user_id: str) -> tuple[list[User], dict]:
+        return self.index(form, only_followers=True, user_id=user_id)
+    
+    def followeds_index(self, form: QueryForm, user_id: str) -> tuple[list[User], dict]:
+        return self.index(form, only_followeds=True, user_id=user_id)
+    
+    def toggle_follow(self, follower_id: str, followed_id: str) -> tuple[bool]:
+        follow = Follow.query.filter(
+            db.and_(Follow.follower_id == follower_id, Follow.followed_id == followed_id)
+        ).first()
+
+        if follow is None:
+            follow = Follow()
+            follow.follower_id = follower_id
+            follow.followed_id = followed_id
+            db.session.add(follow)
+            is_following = True
+        else: 
+            db.session.delete(follow)
+            is_following = False
+        
+        db.session.commit() 
+        return is_following
+
