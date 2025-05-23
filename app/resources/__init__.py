a127f762d30a486362d004fe07537726e5f48152
Muhammad Arfan <arfan2173@gmail.com>
2025-05-23 15:14:00 +0700

    registered resources

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -1,3 +1,5 @@
 from .resource import Resource
 from .user_resource import UserResource
 from .country_resource import CountryResource
+from .role_resource import RoleResource
+from .permission_resource import PermissionResource
