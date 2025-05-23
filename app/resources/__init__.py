2619648aa5a3d3f5811a7c5c81cafb1fe88a41bc
Muhammad Arfan <arfan2173@gmail.com>
2025-05-23 22:23:39 +0700

    registered NotificationResource

--- app/resources/__init__.py
+++ app/resources/__init__.py
@@ -3,3 +3,4 @@ from .user_resource import UserResource
 from .country_resource import CountryResource
 from .role_resource import RoleResource
 from .permission_resource import PermissionResource
+from .notification_resource import NotificationResource
