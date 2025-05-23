55174d6b628ad3ebd82e192951b872eeb41eb3a4
Muhammad Arfan <arfan2173@gmail.com>
2025-05-23 21:52:34 +0700

    updated, from role_id into role_name

--- app/forms/assign_user_role.py
+++ app/forms/assign_user_role.py
@@ -1,5 +1,6 @@
 from wtforms import Form, StringField, validators
+from app.enums.role_enums import RoleName 
 
 class AssignUserRoleForm(Form):
     user_id = StringField('User Id', [validators.DataRequired(), validators.Length(min=26, max=26)])
-    role_id = StringField('Role Id', [validators.DataRequired(), validators.Length(min=26, max=26)])
+    role_name = StringField('Role Name', [validators.DataRequired(), validators.AnyOf([RoleName.ANALYST.value, RoleName.USER.value])])
