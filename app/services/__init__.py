fdf65feff22053e17473e23d1cf7a119dfe4d17f
Muhammad Arfan <arfan2173@gmail.com>
2025-05-23 13:55:28 +0700

    registered RoleService

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -2,3 +2,4 @@ from .service import Service
 from .user_service import UserService
 from .otp_service import OtpService
 from .follow_service import FollowService   
+from .role_service import RoleService   
