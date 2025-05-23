5283770fb84de5a4772b72df16c35d82f26e73da
Muhammad Arfan <arfan2173@gmail.com>
2025-05-23 21:52:42 +0700

    Implemented role functionalities and some improvement: 	modified:   app/controllers/apis/role_controller.py 	modified:   app/services/role_service.py

--- app/controllers/apis/role_controller.py
+++ app/controllers/apis/role_controller.py
@@ -4,7 +4,7 @@ from app.forms import QueryForm, AssignUserRoleForm
 from app.resources import RoleResource  
 from app.services import RoleService
 from app.helpers.response_helpers import create_response_tuple
-from flask import Blueprint, request
+from flask import Blueprint, request, g
 from http import HTTPStatus
 
 role_service = RoleService()
@@ -30,8 +30,8 @@ def index():
 @authenticated
 @authorized(PermissionName.ROLES_SHOW.value)
 @email_verified
-def show(role_id: str):
-    role, = role_service.show(role_id=role_id)
+def show(role_identifier: str):
+    role, = role_service.show(role_identifier=role_identifier)
     role_json = RoleResource(role).to_json()
     return create_response_tuple(status=HTTPStatus.OK, message=f"Role retrieved successfully", data={'role': role_json})
 
@@ -41,13 +41,21 @@ def show(role_id: str):
 @authorized(PermissionName.ROLES_UPDATE.value)
 @email_verified
 def assign_to_user(user_id: str):
-    print('==============================')
-    print('user_id', user_id)
-    form = AssignUserRoleForm(data={user_id: user_id ,**request.form})
+    form = AssignUserRoleForm(request.form)
     form.user_id.data = user_id
     form.try_validate()
 
     role_service.assign_to_user(form=form)
 
     return create_response_tuple(status=HTTPStatus.OK, message=f"Role assigned to user successfully")
-    
+
+@role_bp.route('/users/self/request-analyst', methods=['POST'])
+@api_key_verified
+@authenticated
+@email_verified
+def self_request_analyst():
+    user_id = g.user.id 
+
+    role_service.request_analyst(user_id=user_id)
+
+    return create_response_tuple(status=HTTPStatus.OK, message=f"Analyst request sent successfully")
--- app/services/role_service.py
+++ app/services/role_service.py
@@ -1,9 +1,12 @@
 from app.services import Service
 from app.helpers.mail_helpers import send_mail
-from app.models import Role, User, Permission
+from app.enums.role_enums import RoleName
+from app.enums.notification_enums import NotifiableType, Type as NotificationType
+from app.models import Role, User, Notification
 from app.forms import QueryForm, AssignUserRoleForm
 from app.extensions import db
 from app.exceptions import HttpException, ValidationException
+from datetime import datetime, timedelta
 from http import HTTPStatus
 
 class RoleService(Service):
@@ -24,34 +27,81 @@ class RoleService(Service):
             raise HttpException(message='Roles not found', status=HTTPStatus.NOT_FOUND)
         return (roles, )
     
-    def show(self, role_id: str) -> tuple[Role]:
-        role = Role.query.options(db.joinedload(Role.permissions)).filter_by(id=role_id).first()
+    def show(self, role_identifier: str) -> tuple[Role]:
+        role = Role.query\
+            .options(db.joinedload(Role.permissions))\
+            .filter(
+                db.or_(Role.id == role_identifier, Role.name == role_identifier)
+            ).first()
         if role is None:
             raise HttpException(message='Role not found', status=HTTPStatus.NOT_FOUND)
         return (role, )
     
     def assign_to_user (self, form: AssignUserRoleForm) -> tuple[Role, User]: 
         user_id = form.user_id.data
-        role_id = form.role_id.data
+        role_name = form.role_name.data
 
         user = User.query.options(db.joinedload(User.roles))\
-            .filter(
-                db.and_(User.id == user_id)
-            ).first()
+            .filter(db.and_(User.id == user_id))\
+            .first()
         
         if user is None:
             raise HttpException(message='User not found', status=HTTPStatus.NOT_FOUND)
+        elif user.roles[0].name == RoleName.ADMIN.value:
+            raise HttpException(message='Cannot assign role to admin user', status=HTTPStatus.FORBIDDEN)
         
-        role = Role.query.filter(Role.id == role_id).first()
+        role = Role.query.filter(Role.name == role_name).first()
 
         if role is None:
             raise HttpException(message='Role not found', status=HTTPStatus.NOT_FOUND)
-
-        if any(r.id == role.id for r in user.roles):
-            raise ValidationException(message='User already has this role', errors={'role_id': ['User already has this role']})
+        elif role.name == RoleName.ADMIN.value:
+            message = 'Cannot assign admin role to user'
+            raise ValidationException(message=message, errors={'role_name': [message]})
+        elif any(r.id == role.id for r in user.roles):
+            message = 'User already has this role'
+            raise ValidationException(message=message, errors={'role_name': [message]})
         
         user.roles = [role]
         db.session.commit()
 
         return role, user
         
+    def request_analyst(self, user_id: str) -> tuple[bool]:
+        admin_role = Role.query.filter(Role.name == RoleName.ADMIN.value).first()
+
+        user = User.query.filter(User.id == user_id).first()
+        if user is None:
+            raise HttpException(message='User not found', status=HTTPStatus.NOT_FOUND)
+        user_json = {'id': user.id, 'name': user.name,' email': user.email}
+
+        does_exist = db.session.query(
+            db.exists().where(
+                db.and_(
+                    Notification.notifiable_id == admin_role.id,
+                    Notification.notifiable_type == NotifiableType.ROLE.value,
+                    Notification.type == NotificationType.REQUEST_ANALYST.value,
+                    Notification.created_at >= datetime.now() - timedelta(days=30)
+                )
+            )
+        ).scalar()
+
+        if does_exist:
+            raise HttpException(
+                # the error message should be descriptive enough for the user to understand
+                # what they need to do to resolve the issue
+                message='You have already requested to be an analyst in the last 30 days',
+                status=HTTPStatus.CONFLICT,
+            )
+        
+        notification = Notification(
+            notifiable_id=admin_role.id,    
+            notifiable_type=NotifiableType.ROLE.value,
+            type=NotificationType.REQUEST_ANALYST.value,
+            title='Request Analyst',
+            message=f"{user.name} ({user.email}) has requested to be an analyst",
+            data={'user': user_json },
+        )
+        db.session.add(notification)
+        db.session.commit()
+
+        return (True,)
