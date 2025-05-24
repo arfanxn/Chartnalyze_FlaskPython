dbbfb83f6c3f6f4a3cc4d32e9567810108d45783
Muhammad Arfan <arfan2173@gmail.com>
2025-05-25 01:49:20 +0700

    added role resource on user resource

--- app/resources/user_resource.py
+++ app/resources/user_resource.py
@@ -24,6 +24,15 @@ class UserResource(Resource):
 
         ins = inspect(entity)
 
+        if 'roles' not in ins.unloaded:
+            from app.resources import RoleResource
+            role = entity.role
+            data['role'] = RoleResource(role).to_json()
+
+        if 'permissions' not in ins.unloaded:
+            from app.resources import PermissionResource
+            data['permissions']  = PermissionResource.collection(entity.permissions)
+
         if 'avatar' not in ins.unloaded:
             avatar = entity.avatar    
             data['avatar_url']  = f"{Config.APP_URL}/public/images/avatars/{avatar.file_name}" if avatar is not None else None
