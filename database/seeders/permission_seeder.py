4bc37c2a366e2f176cdbe858feb68661aaa3693a
Muhammad Arfan <arfan2173@gmail.com>
2025-05-27 22:56:31 +0700

    refactored

--- database/seeders/permission_seeder.py
+++ database/seeders/permission_seeder.py
@@ -8,30 +8,14 @@ class PermissionSeeder(Seeder):
     def run(self):
         super().run()
         
-        permissionNames = [
-            PermissionName.ALL.value,
+        permissionNames = [perm.value for perm in PermissionName]
 
-            PermissionName.USERS_INDEX.value,
-            PermissionName.USERS_SHOW.value,
-            PermissionName.USERS_STORE.value,
-            PermissionName.USERS_UPDATE.value,
-            PermissionName.USERS_DESTROY.value,
-
-            PermissionName.ROLES_INDEX.value,
-            PermissionName.ROLES_SHOW.value,
-            PermissionName.ROLES_STORE.value,
-            PermissionName.ROLES_UPDATE.value,
-            PermissionName.ROLES_DESTROY.value,
-
-            PermissionName.PERMISSIONS_INDEX.value,
-            PermissionName.PERMISSIONS_SHOW.value,
-            PermissionName.PERMISSIONS_STORE.value,
-            PermissionName.PERMISSIONS_UPDATE.value,
-            PermissionName.PERMISSIONS_DESTROY.value
-        ]
+        permissions = []
         for permissionName in permissionNames:
             permission = Permission()
             permission.name = permissionName
-            db.session.add(permission)
+            permissions.append(permission)
+
+        db.session.bulk_save_objects(permissions)
         db.session.commit()
         
