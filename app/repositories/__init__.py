b5836e25efefa39b71719e9e09dc702f903bbcea
Muhammad Arfan <arfan2173@gmail.com>
2025-05-28 23:23:48 +0700

    registered LikeRepository

--- app/repositories/__init__.py
+++ app/repositories/__init__.py
@@ -7,3 +7,4 @@ from .follow_repository import FollowRepository
 from .watched_asset_repository import WatchedAssetRepository
 from .post_repository import PostRepository
 from .comment_repository import CommentRepository
+from .like_repository import LikeRepository
