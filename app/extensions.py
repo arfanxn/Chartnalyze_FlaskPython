154faeacb9778a6657025dbaadf9acd3bbc81fb7
Muhammad Arfan <arfan2173@gmail.com>
2025-06-05 18:04:22 +0700

    added `from google_auth_oauthlib.flow import Flow` as an extension

--- app/extensions.py
+++ app/extensions.py
@@ -1,3 +1,4 @@
+from google_auth_oauthlib.flow import Flow
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_limiter import Limiter
@@ -8,6 +9,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_pymongo import PyMongo
 from app.config import Config
 
+# Flask extensions
 cors = CORS()
 db = SQLAlchemy()
 mongo = PyMongo()
@@ -15,3 +17,18 @@ jwt = JWTManager()
 limiter = Limiter(key_func=get_remote_address, default_limits=Config.LIMITER_DEFAULT_LIMITS)
 mail = Mail()
 migrate = Migrate()
+
+# Non Flask extensions
+flow = Flow.from_client_config(
+    client_config={
+        "web": {
+            "client_id": Config.GOOGLE_OAUTH_CLIENT_ID,
+            "client_secret": Config.GOOGLE_OAUTH_CLIENT_SECRET,
+            "auth_uri": Config.GOOGLE_OAUTH_AUTH_URI,
+            "token_uri": Config.GOOGLE_OAUTH_TOKEN_URI,
+            "redirect_uris": Config.GOOGLE_OAUTH_REDIRECT_URIS,
+        },
+    },
+    scopes=Config.GOOGLE_OAUTH_SCOPES,
+    redirect_uri=Config.GOOGLE_OAUTH_REDIRECT_URIS[0],
+)
