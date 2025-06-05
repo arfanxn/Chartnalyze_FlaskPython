2f1b48704d6aba535ae0ad3e53acdae54e421ddd
Muhammad Arfan <arfan2173@gmail.com>
2025-06-05 16:21:31 +0700

    removed config

--- app/config.py
+++ app/config.py
@@ -19,8 +19,6 @@ class Config:
     MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
     GOOGLE_OAUTH_CLIENT_ID = os.getenv('GOOGLE_OAUTH_CLIENT_ID')
     GOOGLE_OAUTH_CLIENT_SECRET = os.getenv('GOOGLE_OAUTH_CLIENT_SECRET')
-    OAUTHLIB_RELAX_TOKEN_SCOPE = os.getenv('OAUTHLIB_RELAX_TOKEN_SCOPE', 'True') == 'True'
-    OAUTHLIB_INSECURE_TRANSPORT = os.getenv('OAUTHLIB_INSECURE_TRANSPORT', 'True') == 'True'
 
     # The environment variables that are used without injection
     UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
