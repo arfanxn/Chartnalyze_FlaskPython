663686aecbdb457afe7148607fd660a4a1e0ff2e
Muhammad Arfan <arfan2173@gmail.com>
2025-06-10 16:41:55 +0700

    created `activities` table migration

---
+++ database/migrations/versions/cb0f94e234f5_create_activities_table.py
@@ -0,0 +1,40 @@
+"""create_activities_table
+
+Revision ID: cb0f94e234f5
+Revises: 0398109cca9f
+Create Date: 2025-06-10 11:15:15.666584
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+from app.enums.activity_enums import CauserType, SubjectType, Type
+
+# revision identifiers, used by Alembic.
+revision = 'cb0f94e234f5'
+down_revision = '0398109cca9f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_table('activities',
+        sa.Column('id', sa.CHAR(26), primary_key=True),
+        sa.Column('type', sa.Enum(*[e.value for e in Type], name='types'), nullable=False),
+        sa.Column('description', sa.Text, nullable=False),
+        sa.Column('subject_id', sa.CHAR(26), nullable=True),
+        sa.Column('subject_type', sa.Enum(*[e.value for e in SubjectType], name='subject_types'), nullable=True),
+        sa.Column('causer_id', sa.CHAR(26), nullable=True),
+        sa.Column('causer_type', sa.Enum(*[e.value for e in CauserType], name='causer_types'), nullable=True),
+        sa.Column('properties', sa.JSON, nullable=False),
+        sa.Column('created_at', sa.DateTime, nullable=False, server_default=sa.func.now()),
+        sa.Column('updated_at', sa.DateTime, server_onupdate=sa.func.now())
+    )
+    pass
+
+
+def downgrade():
+    
+    op.drop_table('activities')
+    pass
