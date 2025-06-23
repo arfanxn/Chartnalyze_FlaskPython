a5eadc49ccf862648b4a8d79e11880a4aafa74ec
Muhammad Arfan <arfan2173@gmail.com>
2025-06-23 23:10:02 +0700

    registered the model as extension

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,5 @@
 from google_auth_oauthlib.flow import Flow
+from ultralytics import YOLO
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
 from flask_limiter import Limiter
@@ -19,16 +20,34 @@ mail = Mail()
 migrate = Migrate()
 
 # Non Flask extensions
-flow = Flow.from_client_config(
-    client_config={
-        "web": {
-            "client_id": Config.GOOGLE_OAUTH_CLIENT_ID,
-            "client_secret": Config.GOOGLE_OAUTH_CLIENT_SECRET,
-            "auth_uri": Config.GOOGLE_OAUTH_AUTH_URI,
-            "token_uri": Config.GOOGLE_OAUTH_TOKEN_URI,
-            "redirect_uris": Config.GOOGLE_OAUTH_REDIRECT_URIS,
+flow = None
+candlestick_ml_model = None
+
+def register_extensions(app):
+    cors.init_app(app, resources={r"/*": {"origins": "*"}})
+    db.init_app(app)
+    mongo.init_app(app)
+    jwt.init_app(app)
+    limiter.init_app(app)
+    mail.init_app(app)
+    migrate.init_app(app, db)
+
+    global flow
+    global candlestick_ml_model
+
+    flow = Flow.from_client_config(
+        client_config={
+            "web": {
+                "client_id": Config.GOOGLE_OAUTH_CLIENT_ID,
+                "client_secret": Config.GOOGLE_OAUTH_CLIENT_SECRET,
+                "auth_uri": Config.GOOGLE_OAUTH_AUTH_URI,
+                "token_uri": Config.GOOGLE_OAUTH_TOKEN_URI,
+                "redirect_uris": Config.GOOGLE_OAUTH_REDIRECT_URIS,
+            },
         },
-    },
-    scopes=Config.GOOGLE_OAUTH_SCOPES,
-    redirect_uri=Config.GOOGLE_OAUTH_REDIRECT_URIS[0],
-)
+        scopes=Config.GOOGLE_OAUTH_SCOPES,
+        redirect_uri=Config.GOOGLE_OAUTH_REDIRECT_URIS[0],
+    )
+
+    candlestick_ml_model = YOLO(Config.CANDLESTICK_YOLO_V8_MODEL_PATH.format(root_path=app.root_path))
+    if (candlestick_ml_model is None): raise Exception('candlestick_ml_model is None')
